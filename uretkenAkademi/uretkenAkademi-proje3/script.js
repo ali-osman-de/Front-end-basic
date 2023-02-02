83ac3df37fb91496940632148f6a1796381cda6b
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2023-02-02 14:19:50 +0300

    UAP-3

---
+++ uretkenAkademi/uretkenAkademi-proje3/script.js
@@ -0,0 +1,14 @@
+
+function exampleInputEmail() {
+    var email = prompt("Email: ")
+    console.log(email)
+}
+
+function exampleInputPassword() {
+    var password = prompt("Password: ")
+    console.log(password)
+}
+
+function submitSign() {
+    alert("Sign in continue")
+}
